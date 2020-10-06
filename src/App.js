807c212236926aac4c3a5ceeec6949104e0d67e4
Will brown <wtbrown10@gmail.com>
2020-10-06 17:38:04 -0400

    reading list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import SongList from './Components/SongList';
+import Navbar from './Components/Navbar';
+import BookContextProvider from './contexts/BookContext';
+import BookList from './Components/BookList';
+import NewBookForm from './Components/NewBookForm';
 
 function App() {
-  
   return (
     <div className="App">
-      <SongList />
+      <BookContextProvider>
+        <Navbar />
+        <BookList />
+        <NewBookForm />
+      </BookContextProvider>
     </div>
   );
 }
